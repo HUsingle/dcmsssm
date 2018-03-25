cdbcad210617992f1060fe62db27dbf12fcacdcb
woshizhaoxu <zxgxun@163.com>
2018-03-25 20:49:36 +0800

    提交竞赛实体类

--- src/main/java/com/dcms/model/Competition.java
+++ src/main/java/com/dcms/model/Competition.java
@@ -1,97 +1,120 @@
 package com.dcms.model;
 
+import com.alibaba.fastjson.annotation.JSONField;
+
 import java.sql.Timestamp;
 
 public class Competition {
-    private int cid;
+    private int cid;     //id
     private String name;  //竞赛名称
-    private Timestamp publishTime;
-    private Timestamp competitionTime;
-    private String host;
-    private String file;
-    private String place;
-    private Timestamp applyStart;
-    private Timestamp applyEnd;
-    private int isTeam;
-
-    public void setCid(int cid) {
-        this.cid = cid;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-    public void setPublishTime(Timestamp publishTime) {
-        this.publishTime = publishTime;
-    }
-
-    public void setCompetitionTime(Timestamp competitionTime) {
-        this.competitionTime = competitionTime;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public void setFile(String file) {
-        this.file = file;
-    }
-
-    public void setPlace(String place) {
-        this.place = place;
-    }
-
-    public void setApplyStart(Timestamp applyStart) {
-        this.applyStart = applyStart;
-    }
-
-    public void setApplyEnd(Timestamp applyEnd) {
-        this.applyEnd = applyEnd;
-    }
-
-    public void setIsTeam(int isTeam) {
-        this.isTeam = isTeam;
-    }
+    @JSONField (format="yyyy-MM-dd HH:mm")               //fastjson自带格式化时间方式
+    private Timestamp publishTime;  //发布时间
+    @JSONField (format="yyyy-MM-dd HH:mm")
+    private Timestamp compeStartTime;   //竞赛时间
+    private int compTime;     //竞赛时间   （分钟）
+    private String host;   //主办方
+    private String file;   //竞赛文件
+    private String place;   //竞赛地点
+    @JSONField (format="yyyy-MM-dd HH:mm")
+    private Timestamp applyStart;    //报名开始时间
+    @JSONField (format="yyyy-MM-dd HH:mm")
+    private Timestamp applyEnd;    //报名结束时间
+    private int isTeam;   //团队赛？
+    private int tid;    //教师ID
 
     public int getCid() {
         return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Timestamp getPublishTime() {
         return publishTime;
     }
 
-    public Timestamp getCompetitionTime() {
-        return competitionTime;
+    public void setPublishTime(Timestamp publishTime) {
+        this.publishTime = publishTime;
+    }
+
+    public Timestamp getCompeStartTime() {
+        return compeStartTime;
+    }
+
+    public void setCompeStartTime(Timestamp compeStartTime) {
+        this.compeStartTime = compeStartTime;
+    }
+
+    public int getCompTime() {
+        return compTime;
+    }
+
+    public void setCompTime(int compTime) {
+        this.compTime = compTime;
     }
 
     public String getHost() {
         return host;
     }
 
+    public void setHost(String host) {
+        this.host = host;
+    }
+
     public String getFile() {
         return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
     }
 
     public String getPlace() {
         return place;
     }
 
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
     public Timestamp getApplyStart() {
         return applyStart;
+    }
+
+    public void setApplyStart(Timestamp applyStart) {
+        this.applyStart = applyStart;
     }
 
     public Timestamp getApplyEnd() {
         return applyEnd;
     }
 
+    public void setApplyEnd(Timestamp applyEnd) {
+        this.applyEnd = applyEnd;
+    }
+
     public int getIsTeam() {
         return isTeam;
+    }
+
+    public void setIsTeam(int isTeam) {
+        this.isTeam = isTeam;
+    }
+
+    public int getTid() {
+        return tid;
+    }
+
+    public void setTid(int tid) {
+        this.tid = tid;
     }
 }
